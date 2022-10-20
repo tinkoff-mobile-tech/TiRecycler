1d86271505146cbbcb706d85959da43c6301bf66
Alexey Korshun <lehakorshun@gmail.com>
2022-10-20 21:55:12 +0600

    add check listener for coroutines

--- ti-recycler-coroutines/src/main/java/ru/tinkoff/mobile/tech/ti_recycler_coroutines/base/CoroutinesHolderFactory.kt
+++ ti-recycler-coroutines/src/main/java/ru/tinkoff/mobile/tech/ti_recycler_coroutines/base/CoroutinesHolderFactory.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
 import ru.tinkoff.mobile.tech.ti_recycler.base.HolderFactory
+import ru.tinkoff.mobile.tech.ti_recycler_coroutines.clicks.TiRecyclerCheckChangeFlow
 import ru.tinkoff.mobile.tech.ti_recycler_coroutines.clicks.TiRecyclerItemClicksFlow
 import ru.tinkoff.mobile.tech.ti_recycler_coroutines.clicks.TiRecyclerItemLongClicksFlow
 
@@ -11,6 +12,7 @@ abstract class CoroutinesHolderFactory : HolderFactory {
 
     protected val clicks = TiRecyclerItemClicksFlow()
     protected val longClicks = TiRecyclerItemLongClicksFlow()
+    protected val checkChanges = TiRecyclerCheckChangeFlow()
 
     fun clickPosition(vararg viewType: Int): Flow<Int> {
         return clicks.filter { it.viewType in viewType }
@@ -33,5 +35,11 @@ abstract class CoroutinesHolderFactory : HolderFactory {
         return longClicks
             .filter { it.viewType == viewType && it.view.id == viewId }
             .map { it.position }
+    }
+
+    fun checkChanges(viewType: Int, viewId: Int): Flow<Pair<Int, Boolean>> {
+        return checkChanges
+            .filter { it.viewType == viewType && it.compoundView.id == viewId }
+            .map { it.position to it.compoundView.isChecked }
     }
 }
