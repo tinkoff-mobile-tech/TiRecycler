7b666e6254cbb4c6a29bf5ee754b25db44722faf
Alexey Korshun <lehakorshun@gmail.com>
2022-10-20 20:44:00 +0600

    extract items factory for sample

---
+++ sample/src/main/java/ru/tinkoff/tirecycler/items/ItemsFactory.kt
@@ -0,0 +1,40 @@
+package ru.tinkoff.tirecycler.items
+
+import ru.tinkoff.mobile.tech.ti_recycler.base.ViewTyped
+import ru.tinkoff.tirecycler.R
+
+fun getBaseRecyclerItems(): List<ViewTyped> {
+    return listOf(
+        TextUi(text = "Below you can see examples of usage sample items"),
+        TextUi(text = "In our project these items are basic and we put them in base HolderFactory"),
+        HeaderUi(text = "Header 32sp", textSizeSp = 32f),
+        TextUi(text = "Description - Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+        TextUi(
+            text = "Description - Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            background = R.drawable.bg_rounded_grey
+        ),
+        TextUi(
+            text = "Description - Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            leftIcon = R.drawable.ic_close_blue
+        ),
+        TextUi(
+            text = "Description - Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            leftIcon = R.drawable.ic_close_blue,
+            background = R.drawable.bg_rounded_grey
+        ),
+        HeaderUi(text = "Header 24sp", textSizeSp = 24f),
+        TextUi(text = "Description - Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+        HeaderUi(text = "Header 16sp"),
+        TextUi(text = "Description - Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+        TitleWithSubtitleUi(
+            "Some title",
+            "Some subtitle - Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        ),
+        TitleWithSubtitleUi(
+            "Title + icon",
+            "Description - single line only",
+            icon = R.drawable.ic_close_blue
+        ),
+        MultiChoiceCheckUi("Checkbox", false),
+    )
+}
--- sample/src/main/java/ru/tinkoff/tirecycler/rx/RxDemoActivity.kt
+++ sample/src/main/java/ru/tinkoff/tirecycler/rx/RxDemoActivity.kt
@@ -15,6 +15,7 @@ import ru.tinkoff.tirecycler.items.HeaderUi
 import ru.tinkoff.tirecycler.items.MultiChoiceCheckUi
 import ru.tinkoff.tirecycler.items.TextUi
 import ru.tinkoff.tirecycler.items.TitleWithSubtitleUi
+import ru.tinkoff.tirecycler.items.getBaseRecyclerItems
 
 
 class RxDemoActivity : AppCompatActivity(R.layout.activity_recycler) {
@@ -46,41 +47,5 @@ class RxDemoActivity : AppCompatActivity(R.layout.activity_recycler) {
     override fun onDestroy() {
         super.onDestroy()
         if (!disposable.isDisposed) disposable.dispose()
-    }
-
-    private fun getBaseRecyclerItems(): List<ViewTyped> {
-        return listOf(
-            TextUi(text = "Below you can see examples of usage sample items"),
-            TextUi(text = "In our project these items are basic and we put them in base HolderFactory"),
-            HeaderUi(text = "Header 32sp", textSizeSp = 32f),
-            TextUi(text = "Description - Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
-            TextUi(
-                text = "Description - Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                background = R.drawable.bg_rounded_grey
-            ),
-            TextUi(
-                text = "Description - Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                leftIcon = R.drawable.ic_close_blue
-            ),
-            TextUi(
-                text = "Description - Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                leftIcon = R.drawable.ic_close_blue,
-                background = R.drawable.bg_rounded_grey
-            ),
-            HeaderUi(text = "Header 24sp", textSizeSp = 24f),
-            TextUi(text = "Description - Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
-            HeaderUi(text = "Header 16sp"),
-            TextUi(text = "Description - Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
-            TitleWithSubtitleUi(
-                "Some title",
-                "Some subtitle - Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-            ),
-            TitleWithSubtitleUi(
-                "Title + icon",
-                "Description - single line only",
-                icon = R.drawable.ic_close_blue
-            ),
-            MultiChoiceCheckUi("Checkbox", false),
-        )
     }
 }
