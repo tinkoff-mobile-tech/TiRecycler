a29084105e63c095a2546af66c22ecbdf5ca385a
Alexey Korshun <lehakorshun@gmail.com>
2022-12-27 22:02:44 +0600

    add endless scroll listener

---
+++ ti-recycler-coroutines/src/main/java/ru/tinkoff/mobile/tech/ti_recycler_coroutines/scroll/endlessScrollListener.kt
@@ -0,0 +1,35 @@
+package ru.tinkoff.mobile.tech.ti_recycler_coroutines.scroll
+
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.conflate
+import ru.tinkoff.mobile.tech.ti_recycler_coroutines.util.checkMainThread
+
+fun RecyclerView.endlessScrollFlow(
+    pageSize: Int = DEFAULT_PAGE_SIZE,
+    threshHold: Int = DEFAULT_THRESHOLD
+): Flow<Int> = callbackFlow {
+    checkMainThread()
+    val listener = SimpleEndLessScrollListener(
+        recycler = this@endlessScrollFlow,
+        collector = { itemCount -> trySend(itemCount) },
+        pageSize = pageSize,
+        visibleThreshold = threshHold
+    )
+    addOnScrollListener(listener.scrollListener)
+    awaitClose { listener.stopListen() }
+}
+    .conflate()
+
+
+private class SimpleEndLessScrollListener(
+    recycler: RecyclerView,
+    private val collector: (Int) -> Unit,
+    pageSize: Int,
+    visibleThreshold: Int
+) : BaseEndLessScrollListener(recycler, pageSize, visibleThreshold) {
+
+    override fun loadMore(itemCount: Int) = collector(itemCount)
+}
