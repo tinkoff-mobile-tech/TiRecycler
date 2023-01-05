567816797a3a0f89aaf7149ff4f05bf93846b829
Alexey Korshun <lehakorshun@gmail.com>
2023-01-05 22:27:34 +0600

    extract common scroll listener

--- ti-recycler-rx2/src/main/java/ru/tinkoff/mobile/tech/ti_recycler_rx2/scroll/endlessScrollObservable.kt
+++ ti-recycler-rx2/src/main/java/ru/tinkoff/mobile/tech/ti_recycler_rx2/scroll/endlessScrollObservable.kt
@@ -3,6 +3,8 @@ package ru.tinkoff.mobile.tech.ti_recycler_rx2.scroll
 import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.Observable
 import io.reactivex.Observer
+import ru.tinkoff.mobile.tech.ti_recycler.scroll.DEFAULT_PAGE_SIZE
+import ru.tinkoff.mobile.tech.ti_recycler.scroll.DEFAULT_THRESHOLD
 
 fun RecyclerView.endlessScrollObservable(
     pageSize: Int = DEFAULT_PAGE_SIZE,
@@ -18,18 +20,8 @@ private class SimpleEndlessScrollObservable(
 ) : Observable<Int>() {
 
     override fun subscribeActual(observer: Observer<in Int>) {
-        val listener = SimpleEndLessScrollListener(recycler, observer, pageSize, threshHold)
+        val listener = DisposableEndLessScrollListener(recycler, pageSize, threshHold, observer)
         observer.onSubscribe(listener)
         recycler.addOnScrollListener(listener.scrollListener)
-    }
-
-    class SimpleEndLessScrollListener(
-        recycler: RecyclerView,
-        private val observer: Observer<in Int>,
-        pageSize: Int,
-        visibleThreshold: Int
-    ) : BaseEndLessScrollListener(recycler, pageSize, visibleThreshold) {
-
-        override fun loadMore(itemCount: Int) = observer.onNext(itemCount)
     }
 }
